f312965578b03de135d6fecf40d4fa54d39f0848
Alpay Özer <alpayozer7@gmail.com>
2024-02-12 18:27:49 +0300

    ana sayfa header kısmındaki iconlar düzenlendi

--- src/components/IconButton/index.js
+++ src/components/IconButton/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import { TouchableOpacity } from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
 
-const AddButton = ({ onPress }) => {
+const IconButton = ({ onPress, icon_name }) => {
   return (
     <TouchableOpacity onPress={onPress}>
-      <Ionicons name="add-outline" size={32} />
+      <Ionicons name={icon_name} size={32} />
     </TouchableOpacity>
   );
 };
 
-export default AddButton;
+export default IconButton;
